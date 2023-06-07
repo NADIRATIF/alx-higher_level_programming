1b9be0fcf12ed0816d897b2949eba7abf4a39f66
nadir <nadir.atif.94@gmail.com>
2023-06-06 17:15:23 -0700

    this commit to correct the Zero output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ lastNumber = -lastNumber if number < 0 else lastNumber
 print(f"Last digit of {number:d} is {lastNumber}", end=" ")
 if lastNumber > 5:
     print(f"and is greater than 5")
-elif number < 0 and lastNumber != 0 or lastNumber <= 5:
+elif number < 0 and lastNumber != 0 or lastNumber <= 5 and lastNumber != 0:
     print(f"and is less than 6 and not 0")
 else:
     print(f"and is 0")
