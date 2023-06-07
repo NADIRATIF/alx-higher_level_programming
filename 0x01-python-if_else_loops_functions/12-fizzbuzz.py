b656dc854bf0e05c49146298dcc903515959b67c
nadir <nadir.atif.94@gmail.com>
2023-06-07 01:54:43 -0700

    fizz buzz functionwq

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100 + 1):
+        if i % 3 == 0:
+            print(f"Fizz", end=(" "))
+        elif i % 5 == 0:
+            print(f"Buzz", end=(" "))
+        else:
+            print(i, end=(" "))
