0d11910a086864b2ef879056085459a4bdef0eae
nadir <nadir.atif.94@gmail.com>
2023-06-08 10:44:34 -0700

    this commit for correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
+
 a = 1
 b = 2
-from add_0 import add
-sumR = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, sumR))
+
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
