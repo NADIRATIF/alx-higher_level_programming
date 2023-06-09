cbff5cd31170feb1be643dd2ecdcba41fb78eec9
nadir <nadir.atif.94@gmail.com>
2023-06-08 22:08:55 -0700

    fix pycode style white space bettween operator

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = ""
     for element in my_string:
         if element != 'c' and element != 'C':
-            new_string +=element
+            new_string += element
     return new_string
