8e1acb7d2f601df3e8a6fbb1eab66821261ef94b
nadir <nadir.atif.94@gmail.com>
2023-06-10 01:15:26 -0700

    correction: change the original listwq

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,8 +3,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
 
-    new_list = my_list[:]
+    del my_list[idx]
 
-    del new_list[idx]
-
-    return new_list
+    return my_list
