95bd486d4fec8d588fa894524985f8a044b06a81
nadir <nadir.atif.94@gmail.com>
2023-06-10 00:31:07 -0700

    thix commit to write a function to print the biggest integer of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max_number = my_list[0]
+
+    for item in my_list:
+        if item > max_number:
+            max_number = item
+
+
+    return max_number
