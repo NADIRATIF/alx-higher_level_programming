aea9eea6448b4f906c014d3ccbe59109f4fd1b90
nadir <nadir.atif.94@gmail.com>
2023-06-25 20:28:22 -0700

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for e in my_list:
             if c < x:
-                print(e, end=" ")
+                print(e, end="")
                 c += 1
             else:
                 break
