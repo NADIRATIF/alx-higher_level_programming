f1c3280e5757f472f36a89fc5a719d0444df654c
nadir <nadir.atif.94@gmail.com>
2023-06-29 14:12:54 -0700

    correction documenting

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+""" class Square that defines a square by: (based on 0-square.py) """
 class Square:
+    """ class define a square  """
     def __init__(self, size):
         self.__size = size
-
